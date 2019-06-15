298d7200ed9ea0bb8a7aeb8032c3fc3afccb22a1
user.name:"Mulubwa17 <user.email:"mulubwac@gmail.com>
2019-06-15 16:10:51 +0200

    solution for print-date.js

--- print-date.js
+++ print-date.js
@@ -3,5 +3,4 @@ var today = new Date();
 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var printDate = date + ' ' + time;
-alert(printDate);
-// eg: console.log("Hello World")
+console.log(printDate);
